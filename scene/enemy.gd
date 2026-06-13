4e9db5aed3f01829de404305b7bc34bb7d2c243c
NickyZemenski <53301896+NickyZemenski@users.noreply.github.com>
2026-06-13 18:26:04 +0300

    movement

--- scene/enemy.gd
+++ scene/enemy.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-@onready var character = $Character
+@onready var hero =$"../Character"
+
+var speed :float = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,5 +10,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("leftClick"):
-		character.position = get_viewport().get_mouse_position()
+	_MoveTowards(hero, delta)
+
+
+func _MoveTowards(target,delta)->void:
+	if (self.position != target.position):
+		self.position = self.position.lerp(target.position, speed* delta)
+	
