71277aa6a426a2c47e68e263e03bbe9bcfdde40b
Taiblockzero <taiblockzero@gmail.com>
2026-06-14 02:51:25 +0300

    Add timer which leads to game over screen with retry button

--- scripts/test.gd
+++ scripts/test.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
 @onready var character = $Player
+@onready var timer = $Timer
+@onready var timer_label = $CanvasLayer/TimerLabel
+
+var time_left := 5.0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,5 +14,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# Move player to mouse on click
 	if Input.is_action_just_pressed("leftClick"):
 		character.position = get_viewport().get_mouse_position()
+	# Update timer label
+	var time_left = timer.time_left
+
+	var total_seconds = int(time_left)
+	var minutes = total_seconds / 60
+	var seconds = total_seconds % 60
+
+	timer_label.text = "%02d:%02d" % [minutes, seconds]
+
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://scene/GameOver.tscn")
