245a15536d9662b68439f124841140a5f590f8c0
NickyZemenski <53301896+NickyZemenski@users.noreply.github.com>
2026-06-14 14:04:37 +0300

    third movement

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,18 +2,22 @@ extends Node2D
 
 @onready var hero =$"../Player"
 
-var speed :float = 150
+var heroPosition = 0
 
-var launchSpeed :float = 415
+var speed :float = 180
+
+var launchSpeed :float = 80
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	heroPosition = hero.position
+	 # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	_MoveTowards(hero, delta)
-
+	#_MoveTowards(hero, delta)
+	pass
+	
 func _MoveTowards(target,delta)->void:
 	if (position != target.position):
 		var direction: Vector2 = global_position.direction_to(target.global_position)
@@ -26,7 +30,15 @@ func _MoveTowardsTwo(target,delta)->void:
 	if (position != target.position):
 		position = position.lerp(target.position, speed* delta)
 		
-func _MoveTowardsThree(target,delta)->void:
-	#if (position != target.position):
-	pass
+func _MoveTowardsThree(target)->void:
+	if (position != target):
+		var direction: Vector2 = global_position.direction_to(target)
+		global_position += direction * launchSpeed
+
+
 	
+
+
+func _on_jump_timer_timeout() -> void:
+	_MoveTowardsThree(heroPosition)
+	heroPosition = hero.position
