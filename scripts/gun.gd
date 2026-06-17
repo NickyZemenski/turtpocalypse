cddfbe59d807761bb32c89b1f955baddf6c600af
AuctoremBG <emielregisbg@gmail.com>
2026-06-17 19:08:09 +0300

    + shooting logic
    added shooting logic

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var bullet_scene: PackedScene
+@onready var muzzle = $Muzzle
 @onready var gun: AnimatedSprite2D = $AnimatedSprite2D
 
 
@@ -13,3 +15,15 @@ func _process(delta: float) -> void:
 	var mousePos = get_global_mouse_position()
 	
 	gun.flip_v = mousePos.x < get_parent().global_position.x
+	
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
+func shoot():
+	var bullet = bullet_scene.instantiate()
+	bullet.global_position = muzzle.global_position
+	bullet.direction = (
+		get_global_mouse_position() - muzzle.global_position
+	).normalized()
+	
+	get_tree().current_scene.add_child(bullet)
